b7fc36f991917414b4bace49f2408a672c62b544
Dheeraj <Dheeraj@192.168.2.6>
2016-09-20 20:37:10 +0530

    wrong path of directory - corrected

--- soundtrip/src/main/java/com/soundtrip/common/CommonConstants.java
+++ soundtrip/src/main/java/com/soundtrip/common/CommonConstants.java
@@ -3,8 +3,8 @@ package com.soundtrip.common;
 public final class CommonConstants {
 
 	// uncomment this for local
-	 public static final String IMAGE_DIRECTORY_LOCAL_PATH = "D:\\Dheeraj\\temp\\";
+//	 public static final String IMAGE_DIRECTORY_LOCAL_PATH = "D:\\Dheeraj\\temp\\";
 	
 	// uncomment this for production
-//	public static final String IMAGE_DIRECTORY_LOCAL_PATH = "/usr/local/";
+	public static final String IMAGE_DIRECTORY_LOCAL_PATH = "/usr/local/";
 }
